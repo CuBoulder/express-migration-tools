d154f4a01d21f772202146a46381706dcfaab260
Tirazel <valion@gmail.com>
2023-08-08 16:50:39 -0600

    Update

--- migrate.py
+++ migrate.py
@@ -11,67 +11,153 @@ parser.add_argument('-s', '--site', help='Sitename')
 parser.add_argument('--extract-sql-from-remote', action='store_true', help='Extract SQL from Pantheon')
 parser.add_argument('--extract-files-from-remote', action='store_true', help='Extract files from Pantheon')
 parser.add_argument('--extract-psa-from-remote', action='store_true', help='Extract files from Pantheon')
+parser.add_argument('--delete-users', action='store_true', help='Delete users')
 
 args = parser.parse_args()
 
+def run_command(cmd):
+    print('Commands: ' + cmd)
+    output = subprocess.run([cmd], shell=True, capture_output=True, text=True)
+    #print(output.stdout)
+
+    if len(output.stderr) > 0:
+        print(output.stderr)
+
+    return output
 
 def create_local_db(sitename):
-    sitename_clean = sitename.replace('-','')
-    cmd = f'mariadb -u root -ppass -e "CREATE DATABASE IF NOT EXISTS {sitename_clean};"'
-    output = subprocess.run([cmd], shell=True, capture_output=True)
-    print(output.stdout)
-    print(output.stderr)
+    print(f'Creating local DB "{sitename}"...')
+    sitename_clean = sitename.replace('-', '')
+    run_command(f'mariadb -u root -ppass -e "CREATE DATABASE IF NOT EXISTS {sitename_clean};"')
+    # output = subprocess.run([cmd], shell=True, capture_output=True)
+    # print(output.stdout)
+    # print(output.stderr)
 
 def extract_sql_from_remote(sitename):
-    print(sitename)
+    print(f'Extract remote SQL...')
     os.makedirs(f'sites/{sitename}', exist_ok=True)
-    cmd = f'terminus remote:drush {sitename}.live -- sql-dump --skip-tables-list=cache,cache_* > sites/{sitename}/database.sql'
-    output = subprocess.run([cmd], shell=True, capture_output=True)
-    print(output.stdout)
-    print(output.stderr)
+    run_command(f'terminus remote:drush {sitename}.live -- sql-dump --skip-tables-list=cache,cache_* > sites/{sitename}/database.sql')
+    # output = subprocess.run([cmd], shell=True, capture_output=True)
+    # print(output.stdout)
+    # print(output.stderr)
 
 def install_drupal(sitename):
-    pass
+    print(f'Installing Drupal...')
+    sitename_clean = sitename.replace('-', '')
+    run_command(f'./sites/{sitename}/code/d --root=sites/{sitename}/code si boulder_profile  --db-url="mysql://root:pass@localhost:3306/{sitename_clean}" --account-name="admin" --account-pass="tiamat.test"  --site-name="{sitename}" install_configure_form.enable_update_status_module=NULL install_configure_form.enable_update_status_emails=NULL --yes')
+    # output = subprocess.run([cmd], shell=True, capture_output=True)
+    # print(output.stdout)
+    # print(output.stderr)
 
-def delete_users(sitename):
-    pass
 
 def create_drush_symlink(sitename):
-    pass
+    print(f'Creating drush symlink...')
+    run_command(f'ln -s ./vendor/bin/drush sites/{sitename}/code/d')
+    # output = subprocess.run([cmd], shell=True, capture_output=True)
+    # print(output.stdout)
+    # print(output.stderr)
 
 def create_migrate_express_symlink(sitename):
-    pass
+    print(f'Creating migrate_express symlink...')
+    run_command(f'ln -s ~/Projects/migrate_express sites/{sitename}/code/web/modules/custom/migrate_express')
+    # output = subprocess.run([cmd], shell=True, capture_output=True)
+    # print(output.stdout)
+    # print(output.stderr)
+
+def create_dataxml_symlink(sitename):
+    print(f'Creating data.xml symlink...')
+    run_command(f'ln -s ../../data.xml sites/{sitename}/code/web/data.xml')
+    # output = subprocess.run([cmd], shell=True, capture_output=True)
+    # print(output.stdout)
+    # print(output.stderr)
+
+def create_set_homepage(sitename):
+    print(f'Set homepage to /home...')
+    run_command(f'./sites/{sitename}/code/d --root=sites/{sitename}/code config:set system.site page.front /home --yes')
+    # output = subprocess.run([cmd], shell=True, capture_output=True)
+    # print(output.stdout)
+    # print(output.stderr)
 
 
 def load_sql_to_local_db(sitename):
-    pass
+    print(f'Loading SQL to local source DB...')
+    sitename_clean = sitename.replace('-', '')
+    run_command(f'mariadb -u root -ppass {sitename_clean} < sites/ucb-biden/database.sql')
+    # output = subprocess.run([cmd], shell=True, capture_output=True)
+    # print(output.stdout)
+    # print(output.stderr)
 
 def clone_template(sitename):
-    cmd = f'git clone https://github.com/CuBoulder/tiamat10-project-template.git sites/{sitename}/code'
-    output = subprocess.run([cmd], shell=True, capture_output=True)
-    print(output.stdout)
-    print(output.stderr)
+    print(f'Clone template...')
+    run_command(f'git clone https://github.com/CuBoulder/tiamat10-project-template.git sites/{sitename}/code')
+    # output = subprocess.run([cmd], shell=True, capture_output=True)
+    # print(output.stdout)
+    # print(output.stderr)
+
+def composer_update(sitename):
+    print(f'Running composer update...')
+    run_command(f'composer update --working-dir=sites/{sitename}/code')
+    # output = subprocess.run([cmd], shell=True, capture_output=True)
+    # print(output.stdout)
+    # print(output.stderr)
 
 def set_files_permissions(sitename):
-    pass
+    print(f'Setting files directory permissions...')
+    run_command(f'chmod -R 777 sites/{sitename}/code/web/sites/default/files')
+    # output = subprocess.run([cmd], shell=True, capture_output=True)
+    # print(output.stdout)
+    # print(output.stderr)
 
 def extract_files_from_remote(sitename):
+    print('Extracting files...')
     os.makedirs(f'sites/{sitename}/files', exist_ok=True)
 
-    cmd = f'terminus local:getLiveFiles {sitename}.live --overwrite'
-    output = subprocess.run([cmd], shell=True, capture_output=True)
-    print(output.stdout)
-    print(output.stderr)
+    run_command(f'terminus local:getLiveFiles {sitename}.live --overwrite')
+    # output = subprocess.run([cmd], shell=True, capture_output=True)
+    # print(output.stdout)
+    # print(output.stderr)
 
-    cmd = f'tar -xzvf /home/tirazel/pantheon-local-copies/files/{sitename}-files.tgz -C sites/{sitename}'
-    output = subprocess.run([cmd], shell=True, capture_output=True)
-    print(output.stdout)
-    print(output.stderr)
+    run_command(f'tar -xzvf /home/tirazel/pantheon-local-copies/files/{sitename}-files.tgz -C sites/{sitename}')
+    # output = subprocess.run([cmd], shell=True, capture_output=True)
+    # print(output.stdout)
+    # print(output.stderr)
 
     os.rename(f'sites/{sitename}/files_live', f'sites/{sitename}/files')
 
+def generate_dataxml(sitename):
+    print("Generating XML...")
+    run_command(f'python express-extract.py --site={sitename}-src > sites/{sitename}/data.xml')
+    #output = subprocess.run([cmd], shell=True, capture_output=True)
+    #print(output.stdout)
+    #print(output.stderr)
+
+def delete_users(sitename):
+    print('Deleting users...')
+    output = run_command(f'./sites/{sitename}/code/d --root=sites/{sitename}/code sqlq "SELECT GROUP_CONCAT(name) FROM users_field_data"')
+    #output = subprocess.run([cmd], shell=True, capture_output=True)
+    #print(str(output.stdout)[9:-5].split(','))
+
+    print(output.stdout)
+    users = str(output.stdout)[7:-2].split(',')
+
+    for user in users:
+        print(f"  Deleting {user}...")
+        run_command(f'./sites/{sitename}/code/d --root=sites/{sitename}/code user:cancel --reassign-content {user} --yes')
+        # output = subprocess.run([cmd], shell=True, capture_output=True)
+        # print(output.stdout)
+        # print(output.stderr)
+    # print(output.stderr)
+
+def enable_migrate_express(sitename):
+    print("Enable Migrate Express...")
+
+    run_command(f'./sites/{sitename}/code/d --root=sites/{sitename}/code en migrate_express --yes')
 
 
+
+
+if args.delete_users:
+    delete_users(args.site)
 
 if args.extract_sql_from_remote:
     extract_sql_from_remote(args.site)
@@ -84,7 +170,16 @@ if args.extract_psa_from_remote:
     extract_files_from_remote(args.site)
     create_local_db(args.site)
     create_local_db(args.site + '-src')
+    load_sql_to_local_db(args.site)
     clone_template(args.site)
+    composer_update(args.site)
+    create_drush_symlink(args.site)
+    create_migrate_express_symlink(args.site)
+    create_dataxml_symlink(args.site)
+    install_drupal(args.site)
+    generate_dataxml(args.site)
+    delete_users(args.site)
+    enable_migrate_express(args.site)
 
 
 'ln -s ../../data.xml data.xml'
@@ -104,3 +199,4 @@ if args.extract_psa_from_remote:
 './sites/ucb-biden/code/d --root=sites/ucb-biden/code config:set system.site page.front /home --yes'
 
 './sites/ucb-biden/code/d --root=sites/ucb-biden/code cr --yes'
+'chmod -R 777 sites/ucb-biden/code/web/sites/default/files'
