ba340951665bc7d63c3f9eec6be45b579cce0d51
AlanBCole <alantuneit@gmail.com>
2024-07-12 13:09:01 +0200

    linting and added typer to requirements.txt

--- pt.py
+++ pt.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 import yaml
-import rich
+
+# import rich
 import typer
 import concurrent.futures
 import subprocess
@@ -9,8 +10,9 @@ from rich import print
 
 app = typer.Typer()
 
+
 def run_command(cmd):
-    print('Commands: ' + cmd)
+    print("Commands: " + cmd)
     output = subprocess.run([cmd], shell=True, capture_output=True, text=True)
     print(output.stdout)
 
@@ -19,20 +21,24 @@ def run_command(cmd):
 
     return output
 
+
 def fetch_connection_information(site):
 
     cmd_connection_info = f'terminus connection:info {site["dst"]}.live --format=yaml'
     connection_info = yaml.safe_load(run_command(cmd_connection_info).stdout)
     return connection_info
 
+
 def fetch_git_info(site):
 
     cmd_connection_info = f'terminus connection:info {site["dst"]}.dev --format=yaml'
     connection_info = yaml.safe_load(run_command(cmd_connection_info).stdout)
-    return connection_info['git_command']
+    return connection_info["git_command"]
+
 
 def block_on_workflows(site):
     pass
+
 
 def wake_site(site):
 
@@ -40,6 +46,7 @@ def wake_site(site):
     print(cmd_wake_site)
 
     run_command(cmd_wake_site)
+
 
 def remote_backup(site):
     cmd_remote_backup = f'terminus backup:create {site["dst"]}.live'
@@ -53,6 +60,7 @@ def clear_upstream_cache(site):
 
     print(cmd_clear_upstream_cache)
     run_command(cmd_clear_upstream_cache)
+
 
 def deploy_environment(site):
 
@@ -69,7 +77,6 @@ def deploy_environment(site):
     cmd_update_deploy_live = f'terminus  env:deploy {site["dst"]}.live'
     print(cmd_update_deploy_live)
     run_command(cmd_update_deploy_live)
-
 
 
 def deploy_update(site):
@@ -114,6 +121,7 @@ def cache_rebuild(site):
 
     run_command(cmd_cache_rebuild)
 
+
 def add_tag(site):
 
     cmd_add_tag = f'terminus tag:add {site["dst"]} "University of Colorado Boulder" -- "upstream-tiamat"'
@@ -121,36 +129,40 @@ def add_tag(site):
 
     run_command(cmd_add_tag)
 
+
 def print_site(site):
-    print(f"https://www.colorado.edu/{site['path']}, https://live-{site['dst']}.pantheonsite.io")
+    print(
+        f"https://www.colorado.edu/{site['path']}, https://live-{site['dst']}.pantheonsite.io"
+    )
+
 
 def user_config(site):
 
     wake_site(site)
 
     web_users = []
-    web_users.append('jesp3304')
-    web_users.append('mibo7729')
-    web_users.append('alco6164')
-    web_users.append('joni1621')
-    web_users.append('jako6198')
-    web_users.append('titr7839')
-    web_users.append('pabr5825')
+    web_users.append("jesp3304")
+    web_users.append("mibo7729")
+    web_users.append("alco6164")
+    web_users.append("joni1621")
+    web_users.append("jako6198")
+    web_users.append("titr7839")
+    web_users.append("pabr5825")
 
     ux_users = []
-    ux_users.append('linebarg')
-    ux_users.append('niwa4700')
-    ux_users.append('brokaw')
-    ux_users.append('wetu1300')
-    ux_users.append('crafts')
+    ux_users.append("linebarg")
+    ux_users.append("niwa4700")
+    ux_users.append("brokaw")
+    ux_users.append("wetu1300")
+    ux_users.append("crafts")
 
     service_manager_users = []
-    service_manager_users.append('webexpress-manager')
-    'site_manager'
+    service_manager_users.append("webexpress-manager")
+    "site_manager"
 
     service_editor_users = []
-    service_editor_users.append('webexpress-editor')
-    'content_editor'
+    service_editor_users.append("webexpress-editor")
+    "content_editor"
 
     for user in web_users:
         cmd_add_user = f'terminus remote:drush {site["dst"]}.live -- user:create {user}  --mail="{user}@colorado.edu" --yes'
@@ -192,7 +204,7 @@ def user_config(site):
 def saml_config(site):
     git_cmd = fetch_git_info(site)
 
-    cmd_fetch_repo = f'cd ./siterepos && {git_cmd}'
+    cmd_fetch_repo = f"cd ./siterepos && {git_cmd}"
     print(cmd_fetch_repo)
     run_command(cmd_fetch_repo)
 
@@ -201,23 +213,24 @@ def saml_config(site):
     run_command(cmd_update_saml_config)
 
 
-
 def upload_local_files(site):
     wake_site(site)
 
     connection_info = fetch_connection_information(site)
 
-    sftp_parts = connection_info['sftp_command'].split(" ")[3].split('@')
+    sftp_parts = connection_info["sftp_command"].split(" ")[3].split("@")
 
     # print(sftp_parts)
 
     lftp_command = f'lftp -c "open sftp://{sftp_parts[0]}:dummypass@{sftp_parts[1]}:2222; mirror --continue --reverse --delete --parallel=5 . files --verbose"'
 
-
-    cmd_upload_local_files = f'cd ./sites/{site["src"]}/code/web/sites/default/files && {lftp_command}'
+    cmd_upload_local_files = (
+        f'cd ./sites/{site["src"]}/code/web/sites/default/files && {lftp_command}'
+    )
 
     print(cmd_upload_local_files)
     run_command(cmd_upload_local_files)
+
 
 def upload_local_database(site):
     # print(site['dst'])
@@ -233,34 +246,43 @@ def upload_local_database(site):
 
 
 def remove_local_databases(site):
-    dst_sitename_clean = site['src'].replace('-', '')
-    src_sitename_clean = dst_sitename_clean + 'src'
+    dst_sitename_clean = site["src"].replace("-", "")
+    src_sitename_clean = dst_sitename_clean + "src"
 
-    cmd_remove_src_database = f'mariadb -u root -ppass --execute="drop database {src_sitename_clean};"'
+    cmd_remove_src_database = (
+        f'mariadb -u root -ppass --execute="drop database {src_sitename_clean};"'
+    )
     print(cmd_remove_src_database)
     run_command(cmd_remove_src_database)
 
-    cmd_remove_dst_database = f'mariadb -u root -ppass --execute="drop database {dst_sitename_clean};"'
+    cmd_remove_dst_database = (
+        f'mariadb -u root -ppass --execute="drop database {dst_sitename_clean};"'
+    )
     print(cmd_remove_dst_database)
     run_command(cmd_remove_dst_database)
+
 
 def export_local_database(site):
     # print(site['dst'])
 
-    sitename_clean = site['src'].replace('-', '')
+    sitename_clean = site["src"].replace("-", "")
 
     cmd_export_local_database = f'cd ./sites/{site["src"]}/code && mysqldump -u root -ppass {sitename_clean} > database.sql'
 
     print(cmd_export_local_database)
     run_command(cmd_export_local_database)
 
+
 def configure_modules(site):
     # print(site['dst'])
 
-    cmd_configure_modules = f'cd ./sites/{site["src"]}/code && ./d pmu migrate_devel migrate_express'
+    cmd_configure_modules = (
+        f'cd ./sites/{site["src"]}/code && ./d pmu migrate_devel migrate_express'
+    )
 
     print(cmd_configure_modules)
     run_command(cmd_configure_modules)
+
 
 def migrate_import(site):
     # print(site['dst'])
@@ -288,15 +310,16 @@ def create_site(site):
     print(cmd_create_site)
     run_command(cmd_create_site)
 
+
 def deploy_site(site):
     # print(site['dst'])
-
 
     cmd_deploy_site = f'terminus env:deploy {site["dst"]}.test --yes && terminus env:deploy {site["dst"]}.live --yes'
 
     # print(cmd_deploy_site)
     run_command(cmd_deploy_site)
-    
+
+
 def download_site(site):
     cmd_download_site = f'./migrate.py --extract-psa-from-remote --site={site["src"]}'
 
@@ -314,7 +337,7 @@ def prepare_site_sitelist(name: str):
         sitelist = yaml.safe_load(input)
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=25) as executor:
-            for _ in executor.map(create_site, sitelist['sites']):
+            for _ in executor.map(create_site, sitelist["sites"]):
                 pass
 
 
@@ -324,8 +347,9 @@ def deploy_site_sitelist(name: str):
         sitelist = yaml.safe_load(input)
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=25) as executor:
-            for _ in executor.map(deploy_site, sitelist['sites']):
+            for _ in executor.map(deploy_site, sitelist["sites"]):
                 pass
+
 
 @app.command()
 def saml_config_sitelist(name: str):
@@ -333,7 +357,7 @@ def saml_config_sitelist(name: str):
         sitelist = yaml.safe_load(input)
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=25) as executor:
-            for _ in executor.map(saml_config, sitelist['sites']):
+            for _ in executor.map(saml_config, sitelist["sites"]):
                 pass
 
 
@@ -343,8 +367,9 @@ def download_site_sitelist(name: str):
         sitelist = yaml.safe_load(input)
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-            for _ in executor.map(download_site, sitelist['sites']):
+            for _ in executor.map(download_site, sitelist["sites"]):
                 pass
+
 
 @app.command()
 def migrate_import_sitelist(name: str):
@@ -352,8 +377,9 @@ def migrate_import_sitelist(name: str):
         sitelist = yaml.safe_load(input)
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-            for _ in executor.map(migrate_import, sitelist['sites']):
+            for _ in executor.map(migrate_import, sitelist["sites"]):
                 pass
+
 
 @app.command()
 def configure_modules_sitelist(name: str):
@@ -361,8 +387,9 @@ def configure_modules_sitelist(name: str):
         sitelist = yaml.safe_load(input)
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-            for _ in executor.map(configure_modules, sitelist['sites']):
+            for _ in executor.map(configure_modules, sitelist["sites"]):
                 pass
+
 
 @app.command()
 def export_local_database_sitelist(name: str):
@@ -370,8 +397,9 @@ def export_local_database_sitelist(name: str):
         sitelist = yaml.safe_load(input)
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-            for _ in executor.map(export_local_database, sitelist['sites']):
+            for _ in executor.map(export_local_database, sitelist["sites"]):
                 pass
+
 
 @app.command()
 def upload_local_database_sitelist(name: str):
@@ -379,8 +407,9 @@ def upload_local_database_sitelist(name: str):
         sitelist = yaml.safe_load(input)
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-            for _ in executor.map(upload_local_database, sitelist['sites']):
+            for _ in executor.map(upload_local_database, sitelist["sites"]):
                 pass
+
 
 @app.command()
 def upload_local_files_sitelist(name: str):
@@ -388,8 +417,9 @@ def upload_local_files_sitelist(name: str):
         sitelist = yaml.safe_load(input)
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-            for _ in executor.map(upload_local_files, sitelist['sites']):
+            for _ in executor.map(upload_local_files, sitelist["sites"]):
                 pass
+
 
 @app.command()
 def cache_rebuild_sitelist(name: str):
@@ -397,8 +427,9 @@ def cache_rebuild_sitelist(name: str):
         sitelist = yaml.safe_load(input)
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-            for _ in executor.map(cache_rebuild, sitelist['sites']):
+            for _ in executor.map(cache_rebuild, sitelist["sites"]):
                 pass
+
 
 @app.command()
 def remove_local_databases_sitelist(name: str):
@@ -406,8 +437,9 @@ def remove_local_databases_sitelist(name: str):
         sitelist = yaml.safe_load(input)
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-            for _ in executor.map(remove_local_databases, sitelist['sites']):
+            for _ in executor.map(remove_local_databases, sitelist["sites"]):
                 pass
+
 
 @app.command()
 def user_config_sitelist(name: str):
@@ -415,8 +447,9 @@ def user_config_sitelist(name: str):
         sitelist = yaml.safe_load(input)
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-            for _ in executor.map(user_config, sitelist['sites']):
+            for _ in executor.map(user_config, sitelist["sites"]):
                 pass
+
 
 @app.command()
 def remote_backup_sitelist(name: str):
@@ -424,8 +457,9 @@ def remote_backup_sitelist(name: str):
         sitelist = yaml.safe_load(input)
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=300) as executor:
-            for _ in executor.map(remote_backup, sitelist['sites']):
+            for _ in executor.map(remote_backup, sitelist["sites"]):
                 pass
+
 
 @app.command()
 def deploy_update_sitelist(name: str):
@@ -433,7 +467,7 @@ def deploy_update_sitelist(name: str):
         sitelist = yaml.safe_load(input)
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
-            for _ in executor.map(deploy_update, sitelist['sites']):
+            for _ in executor.map(deploy_update, sitelist["sites"]):
                 pass
 
 
@@ -443,8 +477,9 @@ def deploy_environment_sitelist(name: str):
         sitelist = yaml.safe_load(input)
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=300) as executor:
-            for _ in executor.map(deploy_environment, sitelist['sites']):
+            for _ in executor.map(deploy_environment, sitelist["sites"]):
                 pass
+
 
 @app.command()
 def add_tag_sitelist(name: str):
@@ -452,8 +487,9 @@ def add_tag_sitelist(name: str):
         sitelist = yaml.safe_load(input)
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-            for _ in executor.map(add_tag, sitelist['sites']):
+            for _ in executor.map(add_tag, sitelist["sites"]):
                 pass
+
 
 @app.command()
 def print_site_sitelist(name: str):
@@ -461,18 +497,19 @@ def print_site_sitelist(name: str):
         sitelist = yaml.safe_load(input)
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
-            for _ in executor.map(print_site, sitelist['sites']):
+            for _ in executor.map(print_site, sitelist["sites"]):
                 pass
-        
+
 
 @app.command()
 def prepare_site(name: str):
     print(name)
 
+
 @app.command()
 def apply_updates(name: str):
     print(name)
 
+
 if __name__ == "__main__":
     app()
-    
