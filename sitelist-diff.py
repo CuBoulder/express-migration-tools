3ef577f71c3d144fc5ebc6089438d72a694cf72f
Tirazel <valion@gmail.com>
2024-08-15 16:49:37 -0600

    Update

---
+++ sitelist-diff.py
@@ -0,0 +1,32 @@
+import yaml
+import rich
+
+sitelist = {}
+sitelist['sites'] = []
+
+
+with open('sitelists/cohort-1-final.yml') as input:
+    sitelist1 = yaml.safe_load(input)
+
+with open('sitelists/cohort-1-launch-1.yml') as input:
+    sitelist2 = yaml.safe_load(input)
+
+dst1 = []
+dst2 = []
+
+for site in sitelist1['sites']:
+    dst1.append(site['dst'])
+
+for site in sitelist2['sites']:
+    dst2.append(site['dst'])
+
+# print(dst1)
+# print(dst2)
+
+dst3 = [value for value in dst1 if value in dst2]
+
+only1 = [value for value in dst1 if value not in dst2]
+print(only1)
+
+
+
